d9b01de0d03210c265bcc0f7214c08e12dd1df2e
Deborah Rioba <W0478760@nscc.ca>
2023-11-29 16:35:03 -0400

    Updated 11292023 1634

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("test").innerText= "Rioba Website";
+const mangoes = document.createElement ("article");
+mangoes.innerHTML = "I really love mangoes";
+document.getElementById("testDOM").appendChild (mangoes);
+
